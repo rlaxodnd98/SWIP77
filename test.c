73ccfc9b6ae82511fe04622f83db560047effed8
Chansik Yun <chansigi@gmail.com>
2023-08-24 13:24:45 +0900

    .

--- test.c
+++ test.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-void foo(void) { printf("foo\n"); }
-void goo() { printf("goo\n"); }
+// void foo(void) { printf("foo\n"); }
+// void goo() { printf("goo\n"); }
 // 위처럼 사용하는 경우, 잘못된 인자가 전달되는 것을
 // 컴파일러가 확인할 수 없습니다.
 
 int main(void)
 {
+    auto int a; // 지역(자동) 변수
+
     // foo(10);
-    goo(10, 20);
+    // goo(10, 20);
 
     return 0;
 }
