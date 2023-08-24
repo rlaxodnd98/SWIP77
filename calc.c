f24692d5a70edcfcf33e8b02edc440318f841fbb
Chansik Yun <chansigi@gmail.com>
2023-08-24 16:17:51 +0900

    .

--- calc.c
+++ calc.c
@@ -1,7 +1,19 @@
 // calc.c
 // gcc calc.c -c
 
+// External linkage(외부 연결)
+// : 다른 파일에서 접근이 가능합니다.
 int add(int a, int b)
 {
     return a + b;
 }
+
+// External linkage를 갖습니다.
+int global = 10;
+
+// Internal linkage
+// : 다른 파일에서 접근이 불가능합니다.
+static void foo(void) { }
+
+// Internal linkage
+static int s = 100;
