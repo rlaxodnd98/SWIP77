05b54925e5cdeafa8730952423973bae528dd328
Chansik Yun <chansigi@gmail.com>
2023-08-24 11:03:39 +0900

    .

--- 2_namespace2.cpp
+++ 2_namespace2.cpp
@@ -38,22 +38,60 @@ int main()
 }
 #endif
 
+#if 0
 // 3) using 지시어(directive)
 // => 특정 이름 공간에 존재하는 모든 것을
 //    암묵적으로 접근할 수 있습니다.
-
+// : 이름 충돌의 문제가 발생합니다.
+// - 현업에서 절대 사용하지 않는 것이 좋습니다.
 namespace audio {
 namespace mp3 {
     void Play() { }
 }
+
+void foo() { }
+
+}
+
+namespace video {
+void Play() { }
 }
 
 using namespace audio::mp3;
 using namespace std;
+using namespace video;
 
 int main()
 {
     Play();
 
     cout << "Hello, C++" << endl;
+}
+#endif
+
+#include <iostream>
+// using namespace std;
+// using std::count;
+using std::cout;
+using std::endl;
+
+int count = 100;
+
+namespace audio {
+int count = 0;
+
+// void Play() { ++count; }
+// - 같은 이름 공간의 요소를 암묵적으로 접근합니다.
+
+void Play() { ++::count; }
+// C++에서는 전역 공간을 명시적으로 접근할 수 있습니다.
+}
+
+int main()
+{
+
+    // cout << count << endl;
+    // 표준 라이브러리 안에 count 이름의 함수가 존재합니다.
+    // 이름 충돌로 인해서, 컴파일러는 어떤 count를 선택할지
+    // 알 수 없습니다.
 }
