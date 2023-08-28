363b7d0cec806115b475508f4df6c9214da36cf4
Chansik Yun <chansigi@gmail.com>
2023-08-28 11:48:28 +0900

    .

--- 9_레퍼런스4.cpp
+++ 9_레퍼런스4.cpp
@@ -1,8 +1,22 @@
-
+#include <iostream>
+using namespace std;
 
 class Sample {
+public:
+    Sample() { cout << "Sample()" << endl; }
+    ~Sample() { cout << "~Sample()" << endl; }
 };
 
+int main()
+{
+    cout << "-----" << endl;
+    // Sample&& r = Sample();
+    Sample();
+    cout << "-----" << endl;
+    // 임시 객체는 레퍼런스에 의해 참조되면, 레퍼런스의 수명과 동일해집니다.
+}
+
+#if 0
 int main()
 {
     Sample s; // lvalue
@@ -10,3 +24,4 @@ int main()
 
     Sample&& r2 = Sample(); // 임시 객체는 rvalue 입니다.
 }
+#endif
