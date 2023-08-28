b9f57a26d3893753a60e9ad24050252ac67f51a7
Chansik Yun <chansigi@gmail.com>
2023-08-28 13:17:41 +0900

    .

---
+++ 20_객체복사2.cpp
@@ -0,0 +1,43 @@
+// 20_객체복사2.cpp
+#include <iostream>
+using namespace std;
+
+//  1) 깊은 복사
+class User {
+    char* name;
+    int age;
+
+public:
+    User(const User& rhs)
+        : age { rhs.age }
+    {
+        name = new char[strlen(rhs.name) + 1];
+        strcpy(name, rhs.name);
+    }
+
+    User(const char* s, int n)
+        : age { n }
+    {
+        name = new char[strlen(s) + 1];
+        strcpy(name, s);
+    }
+
+    ~User()
+    {
+        delete[] name;
+    }
+
+    void Print()
+    {
+        cout << name << "(" << age << ")" << endl;
+    }
+};
+
+int main()
+{
+    User user { "Tom", 42 };
+    user.Print();
+
+    User other { user };
+    other.Print();
+}
