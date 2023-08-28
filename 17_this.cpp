0a88bba84a7b70c9c47d816c998593b02166c060
Chansik Yun <chansigi@gmail.com>
2023-08-28 10:21:10 +0900

    .

---
+++ 17_this.cpp
@@ -0,0 +1,51 @@
+// 17_this.cpp
+#include <iostream>
+using namespace std;
+
+class Point {
+    int x;
+    int y;
+
+public:
+    // void Point::Set(Point* this, int a, int b)
+    // => thiscall
+    void Set(int a, int b)
+    {
+        cout << "this: " << this << endl;
+        // x = a;
+        this->x = a;
+
+        // y = b;
+        this->y = b;
+    }
+};
+
+void Set(int a, int b)
+{
+}
+
+int main()
+{
+    Set(10, 20);
+    /*
+        mov     edx, 20
+        mov     ecx, 10
+        call    void Set(int,int)
+    */
+
+    Point pt1, pt2;
+
+    cout << "pt1: " << &pt1 << endl;
+    pt1.Set(10, 20);
+    // Point::Set(&pt1, 10, 20)
+    /*
+        mov     r8d, 20
+        mov     edx, 10
+        lea     rcx, QWORD PTR pt1$[rsp]  ; rcx = &pt1;
+        call    void Point::Set(int,int)
+    */
+
+    cout << "pt2: " << &pt2 << endl;
+    pt2.Set(100, 200);
+    // Point::Set(&pt2, 100, 200)
+}
