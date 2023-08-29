45ae2d74aeb1bafe3d95db523b8fe6e456e68106
Chansik Yun <chansigi@gmail.com>
2023-08-29 13:23:45 +0900

    .

---
+++ 27_추상클래스2.cpp
@@ -0,0 +1,34 @@
+// 27_추상클래스2.cpp
+#include <iostream>
+using namespace std;
+
+class Camera {
+public:
+    void StartRecording() { cout << "Start Recording" << endl; }
+    void StopRecording() { cout << "Stop Recording" << endl; }
+};
+
+class Car {
+    Camera* camera;
+
+public:
+    Car(Camera* p)
+        : camera { p }
+    {
+    }
+
+    void Go()
+    {
+        camera->StartRecording();
+        cout << "Car go" << endl;
+        camera->StopRecording();
+    }
+};
+
+int main()
+{
+    Camera camera;
+    Car car { &camera };
+
+    car.Go();
+}
