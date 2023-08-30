49eec9439bbef04e3e1b0505896ad0286ec9ad37
Chansik Yun <chansigi@gmail.com>
2023-08-30 15:13:10 +0900

    .

--- 33_STL2.cpp
+++ 33_STL2.cpp
@@ -23,7 +23,7 @@ int main()
 //    [begin, end) => 반개 구간
 //     begin: 시작 위치
 //       end: 끝 다음 위치(Past the end)
-
+#if 0
 void PrintArray(int* begin, int* end)
 {
     while (begin != end) {
@@ -41,4 +41,85 @@ int main()
 
     int* p1 = x; // &x[0]
     int* p2 = x + 5; // Past the end
+}
+#endif
+
+#include <vector>
+#include <list>
+#include <deque>
+
+#if 0
+int main()
+{
+    //                   p1         p2
+    //                   |          |
+    // vector<int> v = { 10, 20, 30 };
+    deque<int> v = { 10, 20, 30 };
+
+    auto p1 = v.begin(); // 시작 위치
+    auto p2 = v.end(); // 끝 다음 위치
+
+    while (p1 != p2) {
+        cout << *p1 << endl;
+        ++p1;
+    }
+}
+#endif
+
+// 컨테이너(vector, list, deque)가 제공하는 멤버 함수를 통해
+// 반복자를 꺼내는 것보다, 일반 함수 begin/end를 사용하는 것이
+// 더 좋습니다.
+// => 일반 배열도 반복자의 개념으로 사용할 수 있습니다.
+#if 0
+int main()
+{
+    // vector<int> v = { 10, 20, 30 };
+    int v[] = { 10, 20, 30 };
+
+    auto p1 = begin(v); // 시작 위치
+    auto p2 = end(v); // 끝 다음 위치
+
+    while (p1 != p2) {
+        cout << *p1 << endl;
+        ++p1;
+    }
+}
+#endif
+
+// Java
+//-------
+// vector
+//   find()
+// list
+//   find()
+
+// C++
+//  vector ---->   <반복자> ---->   find() 일반함수
+//  list   ---->
+//  deque  ---->
+
+// 컨테이너 --->   반복자   ---> 알고리즘
+
+// C++은 반복자를 통해서, 모든 컨테이너를 대상으로
+// 동작하는 일반적인 함수를 제공할 수 있습니다.
+// => 알고리즘
+template <typename T>
+void PrintArray(T begin, T end)
+{
+    while (begin != end) {
+        cout << *begin << endl;
+        ++begin;
+    }
+}
+
+int main()
+{
+    // int x[3] = { 10, 20, 30 };
+    vector<int> x = { 10, 20, 30 };
+
+    // x.begin();
+    vector<int>::iterator p = begin(x);
+    // 반복자의 실제 타입
+
+    PrintArray(begin(x), end(x));
 }
