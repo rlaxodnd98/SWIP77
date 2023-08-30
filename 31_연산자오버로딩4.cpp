c1dfa02663d6f010da8405a47b561dd7a85dbf9f
Chansik Yun <chansigi@gmail.com>
2023-08-30 11:19:05 +0900

    ,

---
+++ 31_연산자오버로딩4.cpp
@@ -0,0 +1,77 @@
+// 31_연산자오버로딩4.cpp
+#include <iostream>
+using namespace std;
+
+#if 0
+namespace xstd {
+
+class istream {
+public:
+    istream& operator>>(int& r)
+    {
+        scanf("%d", &r);
+        return *this;
+    }
+
+    istream& operator>>(double& r)
+    {
+        scanf("%lf", &r);
+        return *this;
+    }
+};
+
+istream cin;
+
+}
+
+int main()
+{
+    int n;
+    double d;
+    // cin >> n >> d;
+    // cin.operator>>(n).operator>>(d)
+
+    xstd::cin >> n >> d;
+
+    cout << n << d << endl;
+}
+#endif
+
+class Point {
+    int x;
+    int y;
+
+public:
+    Point(int a, int b)
+        : x { a }
+        , y { b }
+    {
+    }
+
+    friend ostream& operator<<(ostream& os, const Point& pt);
+    friend istream& operator>>(istream& is, Point& pt);
+};
+
+ostream& operator<<(ostream& os, const Point& pt)
+{
+    return os << pt.x << ", " << pt.y;
+}
+
+istream& operator>>(istream& is, Point& pt)
+{
+    return is >> pt.x >> pt.y;
+}
+
+int main()
+{
+    Point pt { 10, 20 };
+
+    cin >> pt;
+    // cin.operator>>(pt);
+    // cin.operator>>(Point&)
+
+    // operator>>(cin, pt)
+    // istream& operator>>(istream& is, Point& pt)
+
+    cout << pt << endl;
+}
